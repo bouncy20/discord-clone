206a9b99ec91bf9132f557a9ea79e641eb437c1b
anas ben lahcen <ouladbenlahcen.anas@ensi.ma>
2024-06-07 11:42:30 +0100

    Initial commit

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
-export async function generateStaticParams() {
-  // Replace with actual dynamic parameters for sign-in
-  return [
-    { signIn: 'example-param1' },
-    { signIn: 'example-param2' },
-    // Add more parameters as needed
-  ];
-}
-
 export default function Page() {
   return <SignIn />;
 }
