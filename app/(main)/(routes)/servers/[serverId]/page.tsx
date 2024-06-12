239b790763f7c4224c59c1dad39652066d0b3412
anas ben lahcen <ouladbenlahcen.anas@ensi.ma>
2024-06-12 15:38:22 +0100

    flowupdate

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,36 @@
-const ServerIdPage = () => {
-    return ( 
-        <div>
-            Server ID Page
-        </div>
-     );
+import { GetStaticProps, GetStaticPaths } from 'next';
+
+interface ServerIdPageProps {
+    serverId: string;
 }
- 
+
+const ServerIdPage: React.FC<ServerIdPageProps> = ({ serverId }) => {
+    return (
+        <div>
+            Server ID Page: {serverId}
+        </div>
+    );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    // Fetch or define your server IDs here. This is an example.
+    const serverIds = ['server1', 'server2', 'server3'];
+
+    const paths = serverIds.map(serverId => ({
+        params: { serverId },
+    }));
+
+    return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const { serverId } = params as { serverId: string };
+
+    return {
+        props: {
+            serverId,
+        },
+    };
+};
+
 export default ServerIdPage;
