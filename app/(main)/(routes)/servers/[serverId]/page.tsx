f5fa85ba36ed28ce5759db7601746894329e4c84
bouncy20 <155726167+bouncy20@users.noreply.github.com>
2024-07-08 19:45:31 +0100

    Update page.tsx

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-const ServerIdPage = () => {
-    return ( 
-=======
 // In your dynamic route component file `/pages/servers/[serverId].tsx`
 
 import { GetStaticProps, GetStaticPaths } from 'next';
@@ -12,11 +8,10 @@ interface ServerIdPageProps {
 
 const ServerIdPage: React.FC<ServerIdPageProps> = ({ serverId }) => {
     return (
->>>>>>> 25e5abc9416e65719d3112a3c4b3c7651faadeab
         <div>
             Server ID Page
         </div>
-     );
+    );
 }
- 
+
 export default ServerIdPage;
